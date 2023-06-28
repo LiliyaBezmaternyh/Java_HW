79ff74d4907bb757880615779c21ef1aec8d9f22
Liliya Bezmaternyh <Liliya1102@yandex.ru>
2023-06-29 00:26:25 +0500

    add

--- src/main/java/Dz1.java
+++ src/main/java/Dz1.java
@@ -16,26 +16,26 @@ public class Dz1 {
 
 
 
-        int i = metod_1(0, 2000);
+        int i = getRandom(0, 2000);
         System.out.println(i);
-        int n = metod_2(i);
+        int n = seniorBit_1(i);
         System.out.println(n);
-        int z = metod_3(i);
+        int z = seniorBit_2(i);
         System.out.println(z);
-        int m = metod_4(i);
+        int m = seniorBit_3(i);
         System.out.println(m);
-        int[] m1 = metod_5(n,i);
+        int[] m1 = multiplesArray(i, n);
         System.out.println(Arrays.toString(m1));
-        int[] m2 = metod_6(n,i);
+        int[] m2 = nonMultiplesArray(i, n);
         System.out.println(Arrays.toString(m2));
 
     }
-    static int metod_1 (int min_n, int max_n){
+    static int getRandom (int min_n, int max_n){
         Random random = new Random();
         System.out.print("Случайное число = ");
         return random.nextInt(min_n, max_n);
     }
-    static int metod_2(int num){
+    static int seniorBit_1(int num){
         int count = 0;
         while (num >= 2)
         {
@@ -45,7 +45,7 @@ public class Dz1 {
         System.out.print("Номер старшего бита = ");
         return count;
     }
-    static int metod_3(int q){
+    static int seniorBit_2(int q){
         int t = 0;
         while(q > 1) {
             q >>= 1;
@@ -54,20 +54,20 @@ public class Dz1 {
         System.out.print("Номер старшего бита = ");
         return t;
     }
-    static int metod_4(int r) {
+    static int seniorBit_3(int r) {
         int t = Integer.toBinaryString(r).length()-1;
         System.out.print("Номер старшего бита n = ");
         return t;
     }
-    static int[] metod_5(int n, int i){
+    static int[] multiplesArray(int a, int b){
         int size = 0;
-        for (int j = i; j < Short.MAX_VALUE; j++){
-            if (j%n == 0) size++;
+        for (int j = a; j < Short.MAX_VALUE; j++){
+            if (j%b == 0) size++;
         }
         int[] m1 = new int[size];
         int k = 0;
-        for (int j = i; j < Short.MAX_VALUE; j++){
-            if (j%n == 0) {
+        for (int j = a; j < Short.MAX_VALUE; j++){
+            if (j%b == 0) {
                 m1[k] = j;
                 k++;
             }
@@ -75,15 +75,15 @@ public class Dz1 {
         return m1;
     }
 
-    static int[] metod_6(int n, int i){
+    static int[] nonMultiplesArray(int a, int b){
         int size = 0;
-        for (int j = Short.MIN_VALUE; j < i; j++){
-            if (j%n != 0) size++;
+        for (int j = Short.MIN_VALUE; j < a; j++){
+            if (j%b != 0) size++;
         }
         int[] m2 = new int[size];
         int k = 0;
-        for (int j = Short.MIN_VALUE; j < i; j++){
-            if (j%n != 0) {
+        for (int j = Short.MIN_VALUE; j < a; j++){
+            if (j%b != 0) {
                 m2[k] = j;
                 k++;
             }
